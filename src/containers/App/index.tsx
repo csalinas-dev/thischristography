b930a51791fe8a73ea5f1c8d48a5b9f3a9ede9ad
Chris Salinas Jr <casjr13@unm.edu>
2019-03-02 16:13:15 -0700

    feat(site): landing page

---
+++ src/containers/App/index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="app">
+        <img src="/assets/logos/SVG/vertical-white.svg" />
+      </div>
+    );
+  }
+}
+
+export default App;
