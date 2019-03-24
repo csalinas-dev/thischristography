76fb738158a02f1bd62b3deddac650ad4e02a7a5
Chris Salinas Jr <casjr13@unm.edu>
2019-03-24 14:41:25 -0600

    content(home): add content

--- src/containers/App/index.tsx
+++ src/containers/App/index.tsx
@@ -5,18 +5,26 @@ import './App.css';
 class App extends React.Component {
   public render() {
     return (
-      <div>
-        <div className="app">
+      <div className="app">
+        <div className="splash">
           <img src="/assets/logos/logo-white.png" />
         </div>
         <Header />
         <main>
           <section className="container">
             <h1>Albuquerque Photographer</h1>
+            <h4><em>#portraits #weddings #landscapes #automotive #lightpainting</em></h4>
             <p>
-              Christopher Salinas Jr. is a native New Mexican, born and raised in Albuquerque. His photography interests include portraits, weddings, landscapes, automotive, and light painting. He is a software engineer for <a href="https://iteamnm.com/" target="_blank">iTEAM Consulting</a>, and is available for sessions on weekday evenings and on weekends. Send all enquiries to:
+              Christopher Salinas Jr. is a native New Mexican, born and raised in Albuquerque. His photography interests include portraits, weddings, landscapes, automotive, and light painting. He is a software engineer for <a href="https://iteamnm.com/" target="_blank">iTEAM Consulting</a>, and is available for sessions on weekday evenings (after 5:30pm) and weekends. You may contact Chris via email at:
             </p>
             <h3><a href="mailto:thischristography@gmail.com">thischristography@gmail.com</a></h3>
+          </section>
+          <section className="container">
+            <h1>Albuquerque at Sunset</h1>
+            <h3>March 23rd, 2019 &bull; Click to download</h3>
+            <a className="image" href="/assets/images/full-res/ThisChristography-20190323-003.jpg" download="ThisChristography-20190323-003.jpg">
+              <img src="/assets/images/ThisChristography-20190323-003.jpg" alt="Albuquerque"/>
+            </a>
           </section>
         </main>
       </div>
