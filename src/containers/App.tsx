5af43e93d4d4d0286d4dcb396d8ed3655be60bbf
Chris Salinas Jr <casjr13@unm.edu>
2019-04-13 19:05:29 -0600

    feat(NotFound): add 404 page

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 import Footer from '../components/Footer';
 import Home from '../pages/Home';
+import NotFound from '../pages/NotFound';
 import './App.css';
 
 class App extends React.Component {
@@ -9,6 +10,7 @@ class App extends React.Component {
     return (
       <Router>
         <Route path="/" exact={true} component={Home} />
+        <Route component={NotFound} />
         <Footer />
       </Router>
     );
