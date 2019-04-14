22dd1876d66125403b4725e79d643180788a867f
Chris Salinas Jr <casjr13@unm.edu>
2019-04-14 02:38:39 -0600

    content(home): update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,19 +12,12 @@ class Home extends React.Component {
         <Header />
         <main>
           <section className="container">
-            <h1>Albuquerque Photographer</h1>
+            <h1>This Christography</h1>
             <h4><em>#portraits #weddings #landscapes #automotive #lightpainting</em></h4>
             <p>
-              Christopher Salinas Jr. is a native New Mexican, born and raised in Albuquerque. His photography interests include portraits, weddings, landscapes, automotive, and light painting. He is a software engineer in Albuquerque, and is available for sessions on weekday evenings (after 5:30pm) and weekends. You may contact Chris via email at:
+              Chris Salinas Jr. is an Albuquerque Photographer, specializing in portraits, weddings, landscapes, automotive, and light painting. He is a native New Mexican, born and raised in Albuquerque. During the day, he is a softare engineer for a local IT company, but he is available for sessions on the weekends and on weekday evenings (after 5:30pm). You may contact Chris via email at:
             </p>
             <h3><a href="mailto:thischristography@gmail.com">thischristography@gmail.com</a></h3>
-          </section>
-          <section className="container">
-            <h1>Albuquerque at Sunset</h1>
-            <h3>March 23rd, 2019 &bull; Click to download</h3>
-            <a className="image" href="/assets/images/full-res/ThisChristography-20190323-003.jpg" download="ThisChristography-20190323-003.jpg">
-              <img src="/assets/images/ThisChristography-20190323-003.jpg" alt="Albuquerque"/>
-            </a>
           </section>
         </main>
       </div>
