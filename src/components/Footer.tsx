78de67e8a61af06f790c94546114bc6d7b8a209a
Chris Salinas Jr <casjr13@unm.edu>
2019-04-28 04:04:36 -0600

    refactor(footer): content

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,19 +1,20 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 import './Footer.css';
 
 class Footer extends React.Component {
   public render() {
     return (
-      <footer>
-        <div className="container">
-          <div className="social-media">
-            <h3>Social Media</h3>
-            <ul>
-              <li>
-                <a href="https://instagram.com/thischristography">Instagram</a>
-              </li>
-            </ul>
-          </div>
+      <footer className="py-5">
+        <div className="container d-flex flex-column align-items-center">
+          <nav className="navbar">
+            <Link className="navbar-brand" to="/">
+              <img src="/assets/logos/icon.png" alt="This Christography Icon" className="d-inline-block d-md-none align-top" height="54" />
+              <img src="/assets/logos/logo.png" alt="This Christography Logo" className="d-none d-md-inline-block align-top" height="54" />
+            </Link>
+            <Link className="nav-link" to="/rates">Rates</Link>
+          </nav>
+          <small><i className="far fa-copyright" /> 2019 This Christography. All Rights Reserved.</small>
         </div>
       </footer>
     );
