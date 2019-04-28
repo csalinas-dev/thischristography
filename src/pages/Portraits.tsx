d994ba994426c15d15a6f76df4e3229e3b4f362b
Chris Salinas Jr <casjr13@unm.edu>
2019-04-28 05:11:23 -0600

    content: portraits and weddings

--- src/pages/Portraits.tsx
+++ src/pages/Portraits.tsx
@@ -15,13 +15,13 @@ class Portraits extends React.Component {
               <div className="col-md-8">
                 <h1>Portraits</h1>
                 <h3>$100 Per Hour</h3>
-                <p>An hour-long portrait photoshoot includes up to one (1) hour of picture taking. The delivery of the final, branded images will be sent via Google Drive, however, you may choose to have a different delivery method, but at an additional cost (<Link to="/delivery-options">see delivery options</Link>).</p>
+                <p>Play sports? Graduating? In need of a new family portrait?</p>
+                <p>From just-for-fun photos to senior portraits, capture moments of your life to share with your friends and family. This Christography will capture all of the beautiful moments that are going on in your life.</p>
               </div>
               <div className="col-md-4">
                 <img src="/assets/gallery/portraits/ThisChristography-20190319-029.jpg" alt="Destiny's Portrait"/>
               </div>
             </div>
-            
           </section>
           <section className="container">
             <div className="row">
@@ -31,9 +31,9 @@ class Portraits extends React.Component {
               <div className="col-md-8">
                 <h1>Smoke Bombs</h1>
                 <h3>$20 each</h3>
-                <p>This includes the use of one smoke bomb that will last approximately 90 seconds. In order to have smoke bomb portraits taken, the photographer <strong>MUST</strong> be notified <em>at least one week in advance</em>. The offered colors include: Black, Blue, Green, Orange, Pink, Purple, Red, White, and Yellow.</p>
-                <p><small>* This is an add-on item. At least one hour-long photoshoot must be purchased in order to qualify.</small></p>
-                <p><small>** A refund will be given for this item if weather or location does not permit the use of smoke bombs.</small></p>
+                <p>Enhance your photos with an artistic smoke bomb. These colored smoke bombs give you approximately 90 seconds of smoke.</p>
+                <p>In order to have smoke bomb portraits taken, the photographer <strong>MUST</strong> be notified <em>at least one week in advance</em>. The offered colors include: Black, Blue, Green, Orange, Pink, Purple, Red, White, and Yellow.</p>
+                <p><small>* A refund will be given for this item if weather or location does not permit the use of smoke bombs.</small></p>
               </div>
             </div>
           </section>
--- src/pages/Weddings.tsx
+++ src/pages/Weddings.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
+import Gallery from 'react-photo-gallery';
 import Header from '../components/Header';
+import { weddings } from './Galleries';
 
 class Weddings extends React.Component {
   public render() {
@@ -9,6 +11,24 @@ class Weddings extends React.Component {
         <main>
           <section className="container">
             <h1>Weddings</h1>
+            <h3>$100 Per Hour</h3>
+            <p>Enjoy your wedding and festivities with the peace of mind that your special day will be captured.</p>
+          </section>
+          <section className="container">
+            <div className="row">
+              <div className="col-md-8">
+                <h1>Engagement Photos</h1>
+                <h3>$100 Per Hour</h3>
+                <p>Let everyone know that you have tied the knot with loving portraits of you and your fiancee.</p>
+              </div>
+              <div className="col-md-4">
+                <img src="/assets/gallery/weddings/ThisChristography-20190316-021.jpg" alt="One of Alex and Serina's engagement photos"/>
+              </div>
+            </div>
+          </section>
+          <section className="container">
+            <h1>Gallery</h1>
+            <Gallery photos={weddings} direction={"column"} />
           </section>
         </main>
       </div>
