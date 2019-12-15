0b24ff008fed6de7d4486c293ad59ce642f2e823
Chris Salinas Jr <chrissalinasjr@gmail.com>
2019-12-15 11:06:12 -0700

    chore(Routes): code cleanup

--- src/components/Routes.tsx
+++ src/components/Routes.tsx
@@ -4,8 +4,6 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 // Pages
 import Automotive from 'src/pages/Automotive';
 import DeliveryOptions from 'src/pages/DeliveryOptions';
-import Album from 'src/pages/Galleries/Album';
-import Galleries from 'src/pages/Galleries/index';
 import Home from 'src/pages/Home';
 import NotFound from 'src/pages/NotFound';
 import Portraits from 'src/pages/Portraits';
@@ -16,27 +14,21 @@ import Weddings from 'src/pages/Weddings';
 // Components
 import ScrollToTop from './ScrollToTop';
 
-class Routes extends React.Component {
-  public render() {
-    return (
-      <Router>
-        <ScrollToTop>
-          <Switch>
-            <Route path="/" exact={true} component={Home} />
-            <Route path="/automotive" component={Automotive} />
-            <Route path="/delivery-options" component={DeliveryOptions} />
-            <Route path="/portraits" component={Portraits} />
-            <Route path="/rates" component={Rates} />
-            <Route path="/real-estate" component={RealEstate} />
-            <Route path="/weddings" component={Weddings} />
-            <Route path="/galleries" exact={true} component={Galleries} />
-            <Route path="/galleries/:album" component={Album} />
-            <Route component={NotFound} />
-          </Switch>
-        </ScrollToTop>
-      </Router>
-    );
-  }
-}
+const Routes = () => (
+  <Router>
+    <ScrollToTop>
+      <Switch>
+        <Route path="/" exact={true} component={Home} />
+        <Route path="/automotive" component={Automotive} />
+        <Route path="/delivery-options" component={DeliveryOptions} />
+        <Route path="/portraits" component={Portraits} />
+        <Route path="/rates" component={Rates} />
+        <Route path="/real-estate" component={RealEstate} />
+        <Route path="/weddings" component={Weddings} />
+        <Route component={NotFound} />
+      </Switch>
+    </ScrollToTop>
+  </Router>
+);
 
 export default Routes;
