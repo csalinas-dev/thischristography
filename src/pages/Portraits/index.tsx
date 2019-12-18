2bfaf95c2d3245846c634883180af7f07c69abaa
Chris Salinas Jr <chrissalinasjr@gmail.com>
2019-12-18 01:14:59 -0700

    refactor(headings): heading style

--- src/pages/Portraits/index.tsx
+++ src/pages/Portraits/index.tsx
@@ -14,7 +14,7 @@ const Portraits: FC = () => (
     <Container>
       <Row>
         <Col xs={12} lg={6} className="d-flex align-items-center justify-content-center">
-          <h1 className="font-montserrat font-weight-light quote">
+          <h1 className="text-center">
             A portrait is not made in the camera but on either side of it.
           </h1>
         </Col>
@@ -22,6 +22,16 @@ const Portraits: FC = () => (
           <Row>
             {map(portraits, Portrait)}
           </Row>
+        </Col>
+      </Row>
+    </Container>
+    <Container as="section">
+      <Row>
+        <Col lg={4} className="offset-lg-2">
+          Image
+        </Col>
+        <Col lg={4}>
+          <h1>Portraits</h1>
         </Col>
       </Row>
     </Container>
