414aa4d0e82d99485415ee319a05bbc6f89af959
Chris Salinas Jr <chrissalinasjr@gmail.com>
2019-12-23 20:57:10 -0700

    content(thunderbirds): add photos

--- src/pages/Galleries/index.tsx
+++ src/pages/Galleries/index.tsx
@@ -3,6 +3,7 @@ import CoverImage from './CoverImage';
 import Layout from 'components/Layout';
 import lvImages from 'shared/data/images/galleries/las-vegas.json';
 import nfImages from 'shared/data/images/galleries/niagara-falls.json';
+import tbImages from 'shared/data/images/galleries/thunderbirds.json';
 import React, { useContext, useEffect } from 'react';
 import { find } from 'lodash';
 import { ImageViewerContext } from 'containers/ImageViewer/context';
@@ -12,6 +13,7 @@ import './index.scss';
 const albuquerque = find(abqImages, ['id', 'TUPMPRfm628t']);
 const niagaraFalls = find(nfImages, ['id', '82mWs4QzBSfA']);
 const lasVegas = find(lvImages, ['id', '3Y2bkrc6wh36']);
+const thunderbird = find(tbImages, ['id', '9vU3Wz2yFyB3']);
 
 const Landscapes = () => {
   const { dispatch } = useContext(ImageViewerContext);
@@ -40,6 +42,11 @@ const Landscapes = () => {
       {lasVegas &&
         <CoverImage images={lvImages} selected={lasVegas}>
           <h1 className="font-house">Las Vegas, NV</h1>
+        </CoverImage>
+      }
+      {thunderbird &&
+        <CoverImage images={tbImages} selected={thunderbird}>
+          <h1 className="font-house">USAF Thunderbirds</h1>
         </CoverImage>
       }
     </Layout>
