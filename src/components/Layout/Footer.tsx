82cdf48c6b0608b38afaed62f7568bb130ec8148
Chris Salinas Jr <chrissalinasjr@gmail.com>
2019-12-30 00:03:45 -0700

    content(footer): dynamic copyright

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -7,23 +7,27 @@ import { map } from 'lodash';
 import { secondaryLinks } from 'components/Routes/links';
 import './Footer.scss';
 
-const Footer: FC = () => (
-  <footer>
-    <Container className="d-flex flex-column align-items-center">
-      <Navbar variant="dark">
-        <Navbar.Brand as={Link} to="/">
-          <Brand />
-        </Navbar.Brand>
-        <Nav>
-          {map(secondaryLinks, PageLink)}
-        </Nav>
-      </Navbar>
-      <small>
-        <i className="far fa-copyright mr-1 d-inline-block" />
-        {new Date().getFullYear()} This Christography. All Rights Reserved.
+const Footer: FC = () => {
+  const begin = 2019;
+  const today = new Date().getFullYear();
+  return (
+    <footer>
+      <Container className="d-flex flex-column align-items-center">
+        <Navbar variant="dark">
+          <Navbar.Brand as={Link} to="/">
+            <Brand />
+          </Navbar.Brand>
+          <Nav>
+            {map(secondaryLinks, PageLink)}
+          </Nav>
+        </Navbar>
+        <small>
+          <i className="far fa-copyright mr-1 d-inline-block" />
+          {today > begin ? `${begin} - ${today}` : begin} This Christography. All Rights Reserved.
       </small>
-    </Container>
-  </footer>
-);
+      </Container>
+    </footer>
+  );
+};
 
 export default Footer;
