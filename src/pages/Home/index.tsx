c60cea5d2367dde13e92648b44afae25d3b6ed9f
Chris Salinas Jr <chrissalinasjr@gmail.com>
2020-06-21 20:41:24 -0600

    feat(home): video bg

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -14,7 +14,7 @@ const Home = () => (
       <p>This Christography is a photographer based in Albuquerque, NM. Specializing in many forms of portraits (individual, couples, graduates, and family), automotive, engagement, weddings, events, and landscapes.</p>
     </div>
     <video autoPlay muted loop id="background">
-      <source src="/assets/home/background.mp4" type="video/mp4" />
+      <source src="/assets/home/background.mp4?v=a361s35v" type="video/mp4" />
     </video>
     <nav className="socials">{map(socials, SocialLink)}</nav>
     <main><Logo /></main>
