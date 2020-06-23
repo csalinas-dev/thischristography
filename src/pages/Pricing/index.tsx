9483f5f1bc15f57f6ca8ee02311712b1326e219e
Chris Salinas Jr <chrissalinasjr@gmail.com>
2020-06-22 23:53:42 -0600

    content(pricing): update pricing

--- src/pages/Pricing/index.tsx
+++ src/pages/Pricing/index.tsx
@@ -6,47 +6,6 @@ import './index.scss';
 const Pricing = () => (
   <Layout className="pricing">
     <Container as="section">
-      <h1>Photoshoot Pricing</h1>
-      <Table responsive striped>
-        <tbody>
-          <tr>
-            <td>Standard Session</td>
-            <td>$100 / hour</td>
-            <td>Any photography session that does not have special pricing below.</td>
-          </tr>
-          <tr>
-            <td>Graduate Portrait Session</td>
-            <td>$75 / hour</td>
-            <td>For high school or college graduates.</td>
-          </tr>
-          <tr>
-            <td>Wedding Photography</td>
-            <td>$300 / hour</td>
-            <td />
-          </tr>
-          <tr>
-            <td>Automotive Session</td>
-            <td>$75 / hour</td>
-            <td>An automotive session will only include photos of the vehicle. If you would like portraits with your vehicle, refer to the standard session for pricing.</td>
-          </tr>
-        </tbody>
-      </Table>
-    </Container>
-    <Container as="section">
-      <h1>Travelling Fees</h1>
-      <p>
-        Using the one-way distance from Albuquerque to the location of the shoot,
-        the round trip fee is $75 for every 50 miles. For example
-      </p>
-      <ul>
-        <li>0 - 50 miles: included in price.</li>
-        <li>50 - 100 miles: $75</li>
-        <li>100 - 150 miles: $150</li>
-        <li>150 - 200 miles: $225</li>
-        <li>and so on...</li>
-      </ul>
-    </Container>
-    <Container as="section">
       <h1>Session Information</h1>
       <p>All photoshoots require a non-refundable booking fee of $50.00 or 50% of the travelling fee, whichever is greater. The amount will go towards your total balance.</p>
       <p>At the end of your session, you may pay the remaining balance:</p>
@@ -68,8 +27,60 @@ const Pricing = () => (
       <p>Final images will not be delivered until payment has been received.</p>
       <p>Checks will not be accepted.</p>
     </Container>
+    <Container as="section">
+      <h1>Photoshoot Pricing</h1>
+      <Table responsive striped>
+        <thead>
+          <tr>
+            <th/>
+            <th>Current</th>
+            <th>Starting July, 2020</th>
+            <th/>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Portrait Session</td>
+            <td>$100</td>
+            <td>$250</td>
+            <td>Portraits, Seniors Portraits, and Family Portraits</td>
+          </tr>
+          <tr>
+            <td>Boudoir Session</td>
+            <td>$100</td>
+            <td>$750</td>
+            <td>More information coming soon. Contact Chris for an in person or virtual meeting to discuss further information.</td>
+          </tr>
+          <tr>
+            <td>Wedding Photography</td>
+            <td colSpan={2}>Starting at $1500</td>
+            <td>More information coming soon. Contact Chris for more information.</td>
+          </tr>
+          <tr>
+            <td>Automotive Session</td>
+            <td>$75</td>
+            <td>$100</td>
+            <td>Exterior and interior photos of your vehicle. Rolling shots are an additional $50.</td>
+          </tr>
+        </tbody>
+      </Table>
+    </Container>
+    <Container as="section">
+      <h1>Travelling Fees</h1>
+      <p>
+        Using the one-way distance from Albuquerque to the location of the shoot,
+        the round trip fee is $75 for every 50 miles. For example
+      </p>
+      <ul>
+        <li>0 - 50 miles: included in price.</li>
+        <li>50 - 100 miles: $75</li>
+        <li>100 - 150 miles: $150</li>
+        <li>150 - 200 miles: $225</li>
+        <li>and so on...</li>
+      </ul>
+    </Container>
     <Container>
-      <label className="mb-5">All pricing effective January 1<sup>st</sup>, 2020</label>
+      <label className="mb-5">New pricing effective July 1<sup>st</sup>, 2020</label>
     </Container>
   </Layout>
 );
