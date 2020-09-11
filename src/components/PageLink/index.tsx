53242a12953c938d681565a1cbc7f787529e236b
Chris Salinas Jr <chrissalinasjr@gmail.com>
2020-09-10 20:09:51 -0600

    fix(page link): handle outbound links

--- src/components/PageLink/index.tsx
+++ src/components/PageLink/index.tsx
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom';
 import { LinkInfo } from 'shared/models/LinkInfo';
 import { Nav } from 'react-bootstrap';
 
-const PageLink = (link: LinkInfo) => (
-  <Nav.Link as={Link} to={link.url} key={link.id}>{link.name}</Nav.Link>
-);
+const PageLink = (link: LinkInfo) => link.url.startsWith("http") 
+  ? <Nav.Link as="a" href={link.url} key={link.id} target="_blank" rel="noreferrer noopener">{link.name}</Nav.Link>
+  : <Nav.Link as={Link} to={link.url} key={link.id}>{link.name}</Nav.Link>;
 
 export default PageLink;
