c268c32160e541e13f8635a6a2f56d367ced7ecd
Chris Salinas Jr <chrissalinasjr@gmail.com>
2022-03-05 11:50:39 -0700

    chore: setup prettier

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,16 +1,16 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: `https://thischristography.com`
+    siteUrl: `https://thischristography.com`,
   },
   plugins: [
     "gatsby-plugin-netlify-cms",
     "gatsby-plugin-emotion",
     "gatsby-plugin-image",
     {
-      resolve: 'gatsby-plugin-google-analytics',
+      resolve: "gatsby-plugin-google-analytics",
       options: {
-        "trackingId": "UA-136005418-1"
-      }
+        trackingId: "UA-136005418-1",
+      },
     },
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
@@ -18,20 +18,20 @@ module.exports = {
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
-        "name": "images",
-        "path": "./src/images/"
+        name: "images",
+        path: "./src/images/",
       },
-      __key: "images"
+      __key: "images",
     },
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
-        "name": "pages",
-        "path": "./src/pages/"
+        name: "pages",
+        path: "./src/pages/",
       },
-      __key: "pages"
-    }
-  ]
+      __key: "pages",
+    },
+  ],
 };
