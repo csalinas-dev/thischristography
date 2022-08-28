5a88c2f9c44ee0441f73a5c6c77a455a6e3c6bb1
Chris Salinas Jr <chrissalinasjr@gmail.com>
2022-08-28 00:37:00 -0600

    chore: update CMS

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,4 +1,19 @@
 import CMS from 'netlify-cms-app';
 import uploadcare from 'netlify-cms-media-library-uploadcare';
 
+// Add UploadCare
 CMS.registerMediaLibrary(uploadcare);
+
+// Set Slug
+CMS.registerEventListener({
+  name: 'preSave',
+  handler: ({ entry }) => {
+   if (entry.get('slug')) {
+     return entry.get('data').set('slug', entry.get('slug'));
+   } else {
+     // you can use any relevant field/logic for generating the initial slug
+     const slug = entry.getIn(['data', 'title'], '').toLowerCase().replace(/[']/g, '').replace(/[.]/g, '-').replace(/[\s]/g, '-')
+     return entry.get('data').set('slug', slug);
+   }
+  },
+});
