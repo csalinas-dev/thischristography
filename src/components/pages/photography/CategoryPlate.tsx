7a6e3d0ca3b0f39552d648e2b9db63709589263e
Chris Salinas Jr <chrissalinasjr@gmail.com>
2022-08-27 23:34:34 -0600

    style(category plates): fix transitions

--- src/components/pages/photography/CategoryPlate.tsx
+++ src/components/pages/photography/CategoryPlate.tsx
@@ -16,7 +16,6 @@ const PlateWrapper = styled(Link)`
   display: flex;
   flex-flow: column nowrap;
   gap: 1rem;
-  width: 20rem;
 
   &,
   &:visited,
@@ -25,36 +24,42 @@ const PlateWrapper = styled(Link)`
     color: inherit;
   }
 
-  @media ${breakpoints.sm} {
-    width: 15rem;
-  }
-
-  @media ${breakpoints.lg} {
-    width: 18rem;
-  }
-
-  &:hover [data-main-image] {
+  &:hover img {
     transform: scale(1.1);
   }
 `;
 
-const imageStyles = css`
+const ImageWrapper = styled.div`
   border-radius: 1rem;
   box-shadow: ${whiteframes.shadows[12]};
+  height: 30rem;
   overflow: hidden;
-  width: 100%;
+  width: 20rem;
+
+  @media ${breakpoints.sm} {
+    height: 22.5rem;
+    width: 15rem;
+  }
+
+  @media ${breakpoints.lg} {
+    height: 27rem;
+    width: 18rem;
+  }
+`;
+
+const imageStyles = css`
   height: 100%;
   object-fit: cover;
-
-  [data-main-image] {
-    transform: scale(1);
-    transition: all ease-in-out 250ms;
-  }
+  transform: scale(1);
+  transition: all ease-in-out 250ms;
+  width: 100%;
 `;
 
 const CategoryPlate: FC<Props> = ({ alt, href, title, thumbnail }) => (
   <PlateWrapper to={href}>
-    <img alt={alt} css={imageStyles} src={thumbnail} />
+    <ImageWrapper>
+      <img alt={alt} css={imageStyles} src={thumbnail} />
+    </ImageWrapper>
     <h2>{title}</h2>
   </PlateWrapper>
 );
