7208e177a450ef42977432d330d7cb92b84059db
Chris Salinas Jr <chrissalinasjr@gmail.com>
2022-08-28 00:38:53 -0600

    chore: slug

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -8,12 +8,8 @@ CMS.registerMediaLibrary(uploadcare);
 CMS.registerEventListener({
   name: 'preSave',
   handler: ({ entry }) => {
-   if (entry.get('slug')) {
-     return entry.get('data').set('slug', entry.get('slug'));
-   } else {
-     // you can use any relevant field/logic for generating the initial slug
-     const slug = entry.getIn(['data', 'title'], '').toLowerCase().replace(/[']/g, '').replace(/[.]/g, '-').replace(/[\s]/g, '-')
-     return entry.get('data').set('slug', slug);
-   }
+    // you can use any relevant field/logic for generating the initial slug
+    const slug = entry.getIn(['data', 'title'], '').toLowerCase().replace(/[\s]/g, '-')
+    return entry.get('data').set('slug', slug);
   },
 });
